9c85ec3c58295f309d9c676ea85f272b51cc8130
vipaak <amazingvipaak@gmail.com>
2022-02-07 18:35:33 +0100

    Primo Commit
    La pallina segue sempre lo stesso percorso. Una volta fatta partire l'animazione non si può fermare o premere nessun altro tasto.

---
+++ HW1-1A-VB/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Finestra
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bottoneTesto = New System.Windows.Forms.Button()
+        Me.bottonePalla = New System.Windows.Forms.Button()
+        Me.spazioTesto = New System.Windows.Forms.RichTextBox()
+        Me.rettangoloPalla = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'bottoneTesto
+        '
+        Me.bottoneTesto.Location = New System.Drawing.Point(13, 13)
+        Me.bottoneTesto.Name = "bottoneTesto"
+        Me.bottoneTesto.Size = New System.Drawing.Size(125, 23)
+        Me.bottoneTesto.TabIndex = 0
+        Me.bottoneTesto.Text = "Stampa Testo"
+        Me.bottoneTesto.UseVisualStyleBackColor = True
+        '
+        'bottonePalla
+        '
+        Me.bottonePalla.Location = New System.Drawing.Point(12, 124)
+        Me.bottonePalla.Name = "bottonePalla"
+        Me.bottonePalla.Size = New System.Drawing.Size(125, 23)
+        Me.bottonePalla.TabIndex = 1
+        Me.bottonePalla.Text = "Muovi Palla"
+        Me.bottonePalla.UseVisualStyleBackColor = True
+        '
+        'spazioTesto
+        '
+        Me.spazioTesto.Location = New System.Drawing.Point(13, 42)
+        Me.spazioTesto.Name = "spazioTesto"
+        Me.spazioTesto.Size = New System.Drawing.Size(125, 76)
+        Me.spazioTesto.TabIndex = 2
+        Me.spazioTesto.Text = ""
+        '
+        'rettangoloPalla
+        '
+        Me.rettangoloPalla.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.rettangoloPalla.Location = New System.Drawing.Point(13, 153)
+        Me.rettangoloPalla.Name = "rettangoloPalla"
+        Me.rettangoloPalla.Size = New System.Drawing.Size(419, 323)
+        Me.rettangoloPalla.TabIndex = 3
+        '
+        'Finestra
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(443, 488)
+        Me.Controls.Add(Me.rettangoloPalla)
+        Me.Controls.Add(Me.spazioTesto)
+        Me.Controls.Add(Me.bottonePalla)
+        Me.Controls.Add(Me.bottoneTesto)
+        Me.Name = "Finestra"
+        Me.Text = "Finestra"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents bottoneTesto As Button
+    Friend WithEvents bottonePalla As Button
+    Friend WithEvents spazioTesto As RichTextBox
+    Friend WithEvents rettangoloPalla As Panel
+End Class
---
+++ HW1-1A-VB/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports System
+Imports System.Threading
+
+Public Class Finestra
+    ' Inizializzo le variabili, le prime due definiscono la dimensione della palla, le successive la sua posizione e le ultime di quanto deve spostarsi
+    Dim ballWidth As Integer = 50
+    Dim ballHeight As Integer = 50
+    Dim ballPosX As Integer = 100
+    Dim ballPosY As Integer = 100
+    Dim moveStepX As Integer = 1
+    Dim moveStepY As Integer = 1
+
+    ' Questo metodo fa apparire il testo nello spazio designato quando viene premuto l'apposito tasto
+    Private Sub bottoneTesto_Click(sender As Object, e As EventArgs) Handles bottoneTesto.Click
+        spazioTesto.Text = "qwertyuiop" & vbCrLf & "asdfghjkl" & vbCrLf & "zxcvbnm"
+    End Sub
+
+    ' Questo metodo fa apparire la palla nel rettangolo e la fa muovere quando viene premuto l'apposito tasto
+    ' Per evitare che sia troppo veloce ogni volta che fa un movimento si ferma per 20 millisecondi
+    Private Sub bottonePalla_Click(sender As Object, e As EventArgs) Handles bottonePalla.Click
+        For i As Integer = 0 To 500
+            MoveBall(sender, e)
+            Thread.Sleep(20)
+        Next
+    End Sub
+
+    ' Questo metodo cambia la posizione della palla nel rettangolo
+    Private Sub MoveBall(sender As Object, e As EventArgs)
+        ballPosX += moveStepX
+        If ballPosX < 0 Or ballPosX + ballWidth > rettangoloPalla.Size.Width Then
+            moveStepX = -moveStepX
+        End If
+        ballPosY += moveStepY
+        If ballPosY < 0 Or ballPosY + ballHeight > rettangoloPalla.Size.Height Then
+            moveStepY = -moveStepY
+        End If
+        rettangoloPalla.Refresh()
+    End Sub
+
+    ' Questo è un metodo che gestisce l'evento rettangoloPalla.Paint
+    Private Sub PaintCircle(sender As Object, e As PaintEventArgs) Handles rettangoloPalla.Paint
+        e.Graphics.Clear(rettangoloPalla.BackColor)
+        e.Graphics.FillEllipse(Brushes.Blue, ballPosX, ballPosY, ballWidth, ballHeight)
+    End Sub
+End Class
